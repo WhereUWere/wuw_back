205644e26654108aa871d06b1edf326ea0caa9cc
Aiden Lee <hoo8799@gmail.com>
2023-04-23 15:34:51 +0900

    feat: add config settings (#5)
    * chore: add rimraf script to package.json
    
    * chore: install @nestjs/config module
    
    * feat: modify configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().required(),
+                DB_USER: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_PORT: Joi.string().required(),
+                DB_URL: Joi.string().required(),
+                DB_NAME: Joi.string().required(),
+            }),
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
