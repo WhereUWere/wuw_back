9de5418f37c354479aa060fb21942e1c41c2bc66
Aiden Lee <hoo8799@gmail.com>
2023-04-23 18:23:09 +0900

    feat: add swagger configs (#7) (#8)

---
+++ src/config/apiConfig.ts
@@ -0,0 +1,9 @@
+import { ApiConfigProps } from './interface/config.interface';
+
+export const api: ApiConfigProps = {
+    nodeEnv: process.env.NODE_ENV as string,
+    apiVersion: process.env.API_VERSION as string,
+    apiPort: parseInt(process.env.API_PORT as string),
+    globalPrefix: `/api/${process.env.API_VERSION}` as string,
+    swaggerPrefix: process.env.SWAGGER_PREFIX as string,
+};
---
+++ src/config/interface/config.interface.ts
@@ -0,0 +1,7 @@
+export interface ApiConfigProps {
+    nodeEnv: string;
+    apiVersion: string;
+    apiPort: number;
+    globalPrefix: string;
+    swaggerPrefix: string;
+}
