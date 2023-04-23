0f18866e8c7694bf48352404082cd6bba2e3e424
AidenLeeeee <hoo8799@gmail.com>
2023-04-23 18:13:47 +0900

    feat: add swagger configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { api } from './config/apiConfig';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    app.setGlobalPrefix(api.globalPrefix);
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('WUW APIs')
+        .setDescription('API docs for WUW project')
+        .setVersion('1.0.0')
+        .addSecurity('Authorization', {
+            type: 'http',
+            scheme: 'bearer',
+        })
+        .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup(api.swaggerPrefix, app, document);
+
+    await app.listen(api.apiPort);
 }
 bootstrap();
