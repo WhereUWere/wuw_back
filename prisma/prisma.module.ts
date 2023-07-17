6399605f9e23280b3d3be4e1e2b9d5340d941c1a
AidenLeeeee <hoo8799@gmail.com>
2023-07-17 19:25:32 +0900

    feat: inject prisma middleware dependency to prisma service

--- prisma/prisma.module.ts
+++ prisma/prisma.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { PrismaMiddleware } from './prisma.middleware';
 
 @Module({
-    providers: [PrismaService],
+    providers: [PrismaService, PrismaMiddleware],
     exports: [PrismaService],
 })
 export class PrismaModule {}
--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,10 +1,17 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaMiddleware } from './prisma.middleware';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor(private readonly prismaMiddleware: PrismaMiddleware) {
+        super();
+    }
+
     async onModuleInit() {
         await this.$connect();
+        this.$use(this.prismaMiddleware.userFindMiddlewareFunc);
+        this.$use(this.prismaMiddleware.profileFindMiddlewareFunc);
     }
 
     async enableShutdownHooks(app: INestApplication) {
