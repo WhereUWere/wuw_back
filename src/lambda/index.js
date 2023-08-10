3122f1cb283bdbb3b7915d39a6cadaf53bc692bb
AidenLeeeee <hoo8799@gmail.com>
2023-08-10 23:55:33 +0900

    refactor: refactor image resize logic

--- src/lambda/index.js
+++ src/lambda/index.js
@@ -2,21 +2,18 @@ const sharp = require('sharp');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
-const transformationOptions = [
-    { name: 'w140', width: 140 },
-    { name: 'w600', width: 600 },
-];
+const transformationOptions = [{ name: 'w180', width: 180 }];
 
 exports.handler = async (event) => {
     try {
         const Key = event.Records[0].s3.object.key;
-        const keyOnly = Key.split('/')[1];
-        console.log(`Image Resizing: ${keyOnly}`);
+        const [directory, path] = Key.split('/');
+        console.log(`Image Resizing: ${path}`);
         const image = await s3.getObject({ Bucket: 'simple-image-server', Key }).promise();
         await Promise.all(
             transformationOptions.map(async ({ name, width }) => {
                 try {
-                    const newKey = `${name}/${keyOnly}`;
+                    const newKey = `${directory}/${path}_${name}`;
                     const resizedImage = await sharp(image.Body).rotate().resize(width).toBuffer();
                     await s3
                         .putObject({
