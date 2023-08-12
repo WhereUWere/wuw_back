93d531df1547475c8fb5ce61dd0da44384e72706
AidenLeeeee <hoo8799@gmail.com>
2023-08-13 00:25:43 +0900

    test: add test code for user service

---
+++ src/config/jest.env.js
@@ -0,0 +1,3 @@
+process.env.AWS_AVATAR_S3_BUCKET = 'test-avatar-s3-bucket';
+process.env.AVATAR_FILE_MAX_SIZE = 5242880;
+process.env.AWS_REGION = 'ap-northeast-2';
--- src/image/test/image.service.spec.ts
+++ src/image/test/image.service.spec.ts
@@ -3,6 +3,14 @@ import { ImageService } from '../image.service';
 import { S3Service } from 'src/s3/s3.service';
 import { ProfileRepository } from 'src/user/repository/profile.repository';
 import { createMock } from '@golevelup/ts-jest';
+import { ProfileNotFoundException } from 'src/lib/exceptions/profile.exception';
+import { GetAvatarRes } from 'src/user/dto/response/get.avatar.res';
+import {
+    ExcessFileMaxSizeException,
+    UnsupportedMimetypeException,
+} from 'src/lib/exceptions/image.exception';
+import { PostAvatarRes } from 'src/user/dto/response/post.avatar.res';
+import { DeleteAvatarRes } from 'src/user/dto/response/delete.avatar.res';
 
 describe('ImageService', () => {
     let imageService: ImageService;
@@ -25,5 +33,76 @@ describe('ImageService', () => {
 
     it('should be defined', () => {
         expect(imageService).toBeDefined();
+    });
+
+    describe('getAvatar', () => {
+        it('getAvatar 가 정의되어 있다.', () => {
+            expect(imageService.getAvatar).toBeDefined();
+        });
+        it('profile 이 존재하지 않을 경우, ProfileNotFoundException 발생', async () => {
+            profileRepository.findAvatarByUserId = jest.fn().mockResolvedValue(null);
+            const result = async () => await imageService.getAvatar(1);
+            expect(result).rejects.toThrowError(new ProfileNotFoundException());
+        });
+        it('profile 에 avatar 가 존재하지 않을 경우, null 을 리턴한다.', async () => {
+            profileRepository.findAvatarByUserId = jest.fn().mockResolvedValue({ avatar: null });
+            const resDto = new GetAvatarRes(null);
+            const result = await imageService.getAvatar(1);
+            expect(result).toStrictEqual(resDto);
+        });
+        it('profile 의 avatar 를 Storage Url 로 변환하여 리턴한다.', async () => {
+            profileRepository.findAvatarByUserId = jest
+                .fn()
+                .mockResolvedValue({ avatar: 'test.jpeg' });
+            s3Service.createS3ObjectFullUrl = jest
+                .fn()
+                .mockImplementation(() => 'https://test.url');
+            const resDto = new GetAvatarRes('https://test.url');
+            const result = await imageService.getAvatar(1);
+            expect(s3Service.createS3ObjectFullUrl).toBeCalledTimes(1);
+            expect(result).toStrictEqual(resDto);
+        });
+    });
+
+    describe('uploadAvatar', () => {
+        it('uploadAvatar 가 정의되어 있다.', () => {
+            expect(imageService.uploadAvatar).toBeDefined();
+        });
+        it('file 의 mimetype 이 image/ 로 시작하지 않을 경우, UnsupportedMimetypeException 발생', async () => {
+            const image = {
+                mimetype: 'application/pdf',
+            } as Express.Multer.File;
+            const result = async () => await imageService.uploadAvatar(1, image);
+            expect(result).rejects.toThrowError(new UnsupportedMimetypeException());
+        });
+        it('file 의 size 가 5MB 를 초과할 경우, ExcessFileMaxSizeException 발생', async () => {
+            const image = {
+                mimetype: 'image/jpeg',
+                size: 1024 * 1024 * 6,
+            } as Express.Multer.File;
+            const result = async () => await imageService.uploadAvatar(1, image);
+            expect(result).rejects.toThrowError(new ExcessFileMaxSizeException());
+        });
+        it('업로드된 file 의 path 를 리턴한다.', async () => {
+            const image = {
+                mimetype: 'image/jpeg',
+                size: 1024 * 1024 * 4,
+            } as Express.Multer.File;
+            profileRepository.doesUserHaveAvatar = jest.fn().mockImplementation(() => false);
+            const resDto = new PostAvatarRes('origin/1/avatar.jpeg');
+            const result = await imageService.uploadAvatar(1, image);
+            expect(result).toStrictEqual(resDto);
+        });
+    });
+
+    describe('deleteAvatar', () => {
+        it('deleteAvatar 가 정의되어 있다.', () => {
+            expect(imageService.deleteAvatar).toBeDefined();
+        });
+        it('삭제한 file 의 path 를 리턴한다.', async () => {
+            const resDto = new DeleteAvatarRes('origin/1/avatar.jpeg');
+            const result = await imageService.deleteAvatar(1);
+            expect(result).toStrictEqual(resDto);
+        });
     });
 });
