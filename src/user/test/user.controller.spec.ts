63126dbe7a412b3881c1d2ab919aaedbd1828af1
AidenLeeeee <hoo8799@gmail.com>
2023-08-12 22:59:09 +0900

    test: add test code for user controller

--- src/user/test/user.controller.spec.ts
+++ src/user/test/user.controller.spec.ts
@@ -6,6 +6,9 @@ import { PatchProfileReq } from '../dto/request/patch.profile.req';
 import { PatchProfileRes } from '../dto/response/patch.profile.res';
 import { GetProfileRes } from '../dto/response/get.profile.res';
 import { ImageService } from 'src/image/image.service';
+import { GetAvatarRes } from '../dto/response/get.avatar.res';
+import { PostAvatarRes } from '../dto/response/post.avatar.res';
+import { DeleteAvatarRes } from '../dto/response/delete.avatar.res';
 
 describe('UserController', () => {
     let userController: UserController;
@@ -51,6 +54,47 @@ describe('UserController', () => {
             const resDto = new PatchProfileRes('test', '01012341234', '19970101', null);
             userService.updateProfile = jest.fn().mockResolvedValue(resDto);
             const result = await userController.updateProfile(1, reqDto);
+            expect(result).toBe(resDto);
+        });
+    });
+
+    describe('getAvatar', () => {
+        it('getAvatar 가 정의되어 있다.', () => {
+            expect(userController.getAvatar).toBeDefined();
+        });
+        it('Service 의 반환값을 리턴한다.', async () => {
+            const resDto = new GetAvatarRes('https://test.image.url');
+            imageService.getAvatar = jest.fn().mockResolvedValue(resDto);
+            const result = await userController.getAvatar(1);
+            expect(result).toBe(resDto);
+        });
+    });
+
+    describe('uploadAvatar', () => {
+        it('uploadAvatar 가 정의되어 있다.', () => {
+            expect(userController.uploadAvatar).toBeDefined();
+        });
+        it('Service 의 반환값을 리턴한다.', async () => {
+            const image = {
+                originalname: 'test.jpeg',
+                mimetype: 'image/jpeg',
+                buffer: Buffer.from('test image buffer'),
+            } as Express.Multer.File;
+            const resDto = new PostAvatarRes('https://test.image.url');
+            imageService.uploadAvatar = jest.fn().mockResolvedValue(resDto);
+            const result = await userController.uploadAvatar(1, image);
+            expect(result).toBe(resDto);
+        });
+    });
+
+    describe('deleteAvatar', () => {
+        it('deleteAvatar 가 정의되어 있다.', () => {
+            expect(userController.deleteAvatar).toBeDefined();
+        });
+        it('Service 의 반환값을 리턴한다.', async () => {
+            const resDto = new DeleteAvatarRes('https://test.image.url');
+            imageService.deleteAvatar = jest.fn().mockResolvedValue(resDto);
+            const result = await userController.deleteAvatar(1);
             expect(result).toBe(resDto);
         });
     });
